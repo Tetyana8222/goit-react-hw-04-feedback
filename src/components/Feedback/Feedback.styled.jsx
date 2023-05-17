b38cb945fecff12ceebe8554e39fe7aead72fe59
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-05-17 08:23:02 +0300

    add src

---
+++ src/components/Feedback/Feedback.styled.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  width: 600px;
+  margin-top: 40px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 20px;
+  background-color: 
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.23), 0px 1px 1px rgba(0, 0, 0, 0.24),
+    0px 2px 1px rgba(0, 0, 0, 0.5);
+  border-radius: 4px;
+`;
